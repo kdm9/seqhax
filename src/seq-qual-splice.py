4b34efb43e185bb3659f93ac95436d4bd4fa63b4
Kevin Murray <spam@kdmurray.id.au>
2015-03-19 14:49:38 +1100

    Rewrite seq-qual-splice.py

--- src/seq-qual-splice.py
+++ src/seq-qual-splice.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+from __future__ import print_function
 import screed
 import sys
 from itertools import izip
@@ -13,11 +15,21 @@ def main():
         seq_len = len(seq.sequence)
         qual_len = len(qual.accuracy)
         if qual_len < seq_len:
-            print >> sys.stderr, "WARNING: truncating", seq.name
+            print("WARNING: truncating", seq.name, file=sys.stderr)
             seq_len = qual_len
-        print "@{}\n{}\n+\n{}".format(seq.name,
-                                      seq.sequence[:seq_len],
-                                      qual.accuracy[:seq_len])
+        out_seq = "@{}\n{}\n+\n{}\n".format(seq.name, seq.sequence[:seq_len],
+                                          qual.accuracy[:seq_len])
+        print(out_seq, end='')
+
+USAGE = """
+USAGE: {} <seq_fastq> <qual_fastq>
+
+Splices the sequence lines from <seq_fastq> with the quality lines of
+<qual_fastq>, truncating both the the shorter of the two.
+"""
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(USAGE.format(sys.argv[0]), file=sys.stderr)
+        exit()
     main()
