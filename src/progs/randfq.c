df777dea7ab175934332d66a96c16fb8b7a84519
Kevin Murray <spam@kdmurray.id.au>
2016-06-23 17:59:40 +1000

    port randfq to new system

---
+++ src/progs/randfq.c
@@ -0,0 +1,134 @@
+/*******************************************************************************
+*                                   randfq                                    *
+*                                                                             *
+*                        Creates random sequence file                         *
+*******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <getopt.h>
+#include <assert.h>
+
+#include <qes_seq.h>
+
+#include "pcg_variants.h"
+
+void
+randfq_usage(FILE *stream)
+{
+    fprintf(stream, "USAGE:\n");
+    fprintf(stream, "    seqhax randfq [options]\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "OPTIONS:\n");
+    fprintf(stream, "    -n READS   Number of reads. Use 0 for infinite. [default 1000]\n");
+    fprintf(stream, "    -s SEED    Seed for RNG.\n");
+    fprintf(stream, "    -l LENGTH  Length of each read. [default 100]\n");
+    fprintf(stream, "    -p         Paired reads [default false]\n");
+    fprintf(stream, "    -f         Output as fasta (no qualities)\n");
+}
+static const char *randfq_optstr = "n:s:l:pf";
+
+static inline void
+randseq(pcg32_random_t *rng, char *seq, size_t len)
+{
+    assert(rng != NULL);
+    assert(seq != NULL);
+    const char NT[] = "ACGT";
+    uint32_t rand;
+
+    for (size_t i = 0; i < len;) {
+        rand = pcg32_random_r(rng);
+        for (size_t shift = 0; shift < 16 && i < len; shift++) {
+            seq[i++] = NT[rand & 0x3];
+            rand >>= 2;
+        }
+    }
+}
+
+int
+randfq_main(int argc, char *argv[])
+{
+    struct qes_seq *seq;
+    size_t n_recs = 0;
+    uint32_t seqlen = 100;
+    uint64_t num_seqs = 1000;
+    pcg32_random_t rng;
+    uint32_t seed;
+    bool paired = false;
+    bool fasta = false;
+    int c;
+
+    pcg32_srandom_r(&rng, 31u, 12u);
+    while ((c = getopt(argc, argv, randfq_optstr)) > 0) {
+        switch (c) {
+            case 'n':
+                num_seqs = strtoull(optarg, NULL, 10);
+                break;
+            case 's':
+                seed = strtoul(optarg, NULL, 10);
+                if (errno != 0) {
+                    fprintf(stderr, "Invalid seed '%s'\n\n", optarg);
+                    randfq_usage(stderr);
+                    return EXIT_FAILURE;
+                }
+                pcg32_srandom_r(&rng, seed, seed + 12);
+                break;
+            case 'l':
+                seqlen = strtoull(optarg, NULL, 10);
+                if (seqlen > UINT32_MAX) {
+                    fprintf(stderr, "Invalid read length '%s'\n\n", optarg);
+                    randfq_usage(stderr);
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'p':
+                paired = true;
+                break;
+            case 'f':
+                fasta = true;
+                break;
+            default:
+                fprintf(stderr, "Invalid argument '%c' (=%s)\n\n", optopt, optarg);
+                randfq_usage(stderr);
+                return EXIT_FAILURE;
+                break;
+        }
+    }
+
+    // Init sequence
+    char *sequence = calloc(seqlen + 1, 1);
+    assert(sequence != NULL);
+
+    seq = qes_seq_create();
+    if (fasta) {
+        qes_seq_fill_qual(seq, "", 0);
+    } else {
+        char *qual = calloc(seqlen + 1, 1);
+        assert(qual != NULL);
+        for (size_t i = 0; i < seqlen; i++) {
+            qual[i] = 'I';
+        }
+        qes_seq_fill_qual(seq, qual, seqlen);
+        free(qual);
+    }
+
+    for (; n_recs < num_seqs; n_recs++) {
+        size_t namelen = 0;
+        char readid[101] = "";
+        if (paired) {
+            namelen = snprintf(readid, 100, "pair:%zu/%zu", n_recs, (n_recs % 2) + 1);
+        } else {
+            namelen = snprintf(readid, 100, "read:%zu", n_recs);
+        }
+        qes_seq_fill_name(seq, readid, namelen);
+
+        randseq(&rng, sequence, seqlen);
+        qes_seq_fill_seq(seq, sequence, seqlen);
+        
+        qes_seq_print(seq, stdout);
+    }
+    qes_seq_destroy(seq);
+    return EXIT_SUCCESS;
+}
---
+++ src/seqhax-main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "progs/randfq.c"
+
+static const char *programs[] = {
+    "randfq  -- generates random sequence files",
+    NULL
+};
+
+struct seqhax_prog {
+    const char *name;
+    int (*mainfunc)(int, char **);
+};
+
+static const struct seqhax_prog program_mains[] = {
+    {"randfq", randfq_main},
+    {NULL, NULL}
+};
+
+
+void
+seqhax_usage(FILE *stream)
+{
+    const char **progname = programs;
+    fprintf(stream, "USAGE:\n");
+    fprintf(stream, "    seqhax PROGRAM [options]\n");
+    fprintf(stream, "\n");
+    fprintf(stream, "where PROGRAM is one of:\n");
+    while (*progname) {
+        fprintf(stream, "    %s\n", *progname);
+        progname++;
+    }
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        seqhax_usage(stdout);
+        return EXIT_SUCCESS;
+    }
+    const struct seqhax_prog *program = program_mains;
+    for (program = program_mains; program->name; program++) {
+        if (strcmp(argv[1], program->name) == 0) {
+            return program->mainfunc(argc - 1, argv + 1);
+        }
+    }
+
+    /* If we get to here, no program matched the program name */
+    fprintf(stderr, "Invalid program name '%s'\n", argv[1]);
+    seqhax_usage(stderr);
+    return EXIT_FAILURE;
+}
