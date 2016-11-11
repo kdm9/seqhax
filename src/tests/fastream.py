8ab215f666c46184bdb94722af0190b019b134a3
Kevin Murray <spam@kdmurray.id.au>
2016-11-11 18:40:32 +1100

    Add fq/fastream

---
+++ src/tests/fastream.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+
+
+MAXSIZE = 10000000
+
+seq = ""
+for i in range(1, MAXSIZE + 1):
+    seq += 'A'
+    print(">", i, "\n", seq, "\n", sep='', end='')
