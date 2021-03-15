e2702ad2e500e2b7be3682010916b45529cfd5f0
Kevin Murray <foss@kdmurray.id.au>
2021-03-15 15:57:09 +1100

    Add updated pecheck

--- scripts/pecheck-wrapper.py
+++ scripts/pecheck-wrapper.py
@@ -11,11 +11,10 @@ from sys import stdout, stderr
 
 
 def run(cmd):
-    print(cmd)
-    return sp.check_call(cmd, shell=True, executable="/bin/bash"),
+    return (cmd, sp.check_call(cmd, shell=True, executable="/bin/bash"))
 
 
-def main(inputs, outdir, dryrun=True, jobs=1, force=False, gzip="gzip", fastqpattern='*.fastq.gz'):
+def main(inputs, outdir, dryrun=True, jobs=1, force=False, gzip="gzip", fastqpattern='*.fastq.gz', rgpattern="_R([12])", lane_pattern=r"_L\d+", expect_n=2):
     if not op.exists(outdir) and not dryrun:
         os.makedirs(outdir)
 
@@ -30,15 +29,18 @@ def main(inputs, outdir, dryrun=True, jobs=1, force=False, gzip="gzip", fastqpat
 
     matching_fastqs = defaultdict(list)
     for fastq in fastqs:
-        sans_r12 = re.sub("_R[12]", "", fastq)
-        matching_fastqs[sans_r12].append(fastq)
+        group = re.sub(rgpattern, "_READGROUPPATTERN", fastq)
+        group = re.sub(lane_pattern, "_LANEPATTERN", group)
+        matching_fastqs[group].append(fastq)
 
     todo = []
     for group, members in matching_fastqs.items():
-        if len(members) != 2:
-            print("ERROR: group of matching files does not contain a pair:", group, members, file=stderr)
+        if len(members) != expect_n:
+            print("ERROR: group of matching files does not contain", expect_n, "files:", group, members, file=stderr)
             continue
-        out = op.basename(group)
+        out = op.basename(group).replace("_READGROUPPATTERN", "_il").replace("_LANEPATTERN", "")
+        if out.endswith(".fq") or out.endswith(".fq.gz"):
+            out = re.sub(r"\.fq(\.gz)?$", ".fastq.gz", out)
         if not out.endswith(".gz"):
             out += ".gz"
         out = op.join(outdir, out)
@@ -58,21 +60,29 @@ def main(inputs, outdir, dryrun=True, jobs=1, force=False, gzip="gzip", fastqpat
             print("No groups found", file=stderr)
         return
     pool = mp.Pool(jobs)
-    res = pool.imap(run, todo)
-    for r in res:
-        if isinstance(r, sp.CalledProcessError):
+    for cmd, res in pool.imap(run, todo):
+        print(cmd)
+        if isinstance(res, sp.CalledProcessError):
             pool.terminate()
-            raise r
+            raise res
 
 
 if __name__ == "__main__":
     a = ap.ArgumentParser("pecheck-wrapper.py")
     a.add_argument("--gzip", type=str, default="gzip",
                    help="Which command should be used to gzip? (try pigz!)")
+    a.add_argument("-L", "--lane-pattern", default=r"_L\d+(?=[_.])", type=str,
+                   help="regex to match the lane number (typcally L1 or L001)")
+    a.add_argument("-R", "--readgroup-pattern", default=r"_R([12](?=[_.])", type=str,
+                   help="regex to match the read group (typcally R1/R2, sometimes just _1/_2)")
+    a.add_argument("-F", "--fastq-pattern", default="*.fastq.gz", type=str,
+                   help="shell glob pattern to match a fastq file.")
     a.add_argument("-j", "--jobs", default=1, type=int,
                    help="number of parallel jobs")
     a.add_argument("-f", "--force", action="store_true",
                    help="Force creation of merged outputs even if they exist")
+    a.add_argument("-N", "--expect-n", default=2, type=int,
+                   help="Expect N files per group.")
     a.add_argument("-n", "--dry-run", action="store_true",
                    help="Don't actually do anything, just print what would be done")
     a.add_argument("OUTPUT", type=str,
@@ -81,4 +91,5 @@ if __name__ == "__main__":
                    help="Input directory(s) or files")
     args = a.parse_args()
     main(args.INPUT, args.OUTPUT, dryrun=args.dry_run, jobs=args.jobs,
-         force=args.force, gzip=args.gzip)
+         force=args.force, gzip=args.gzip, fastqpattern=args.fastq_pattern,
+         rgpattern=args.readgroup_pattern, lane_pattern=args.lane_pattern, expect_n=args.expect_n)
