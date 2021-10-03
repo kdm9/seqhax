b468f65c5be2c8e8d663271c460b7cee8efdb1c3
Kevin Murray <foss@kdmurray.id.au>
2021-10-03 12:42:54 +1100

    htshax bcfhist: add sample missingness calc

--- src/htshax/htshax_bcfhist.cc
+++ src/htshax/htshax_bcfhist.cc
@@ -6,7 +6,9 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <numeric>
+#include <getopt.h>
 #include <htslib/hts.h>
 #include <htslib/vcf.h>
 #include <htslib/synced_bcf_reader.h>
@@ -22,6 +24,9 @@ void usage() {
         << "USAGE:" << endl
         << "    htshax bcfhist BCF_FILE" << endl
         << endl
+        << "OPTIONS:"<< endl
+        << "    -s FILE        Calculate per-sample missingness, output to FILE (as TSV)." << endl
+        << endl
         << "Calculates histograms of allele frequency and missingness across SNPs."
         << endl;
 }
@@ -29,26 +34,36 @@ void usage() {
 
 int bcfhist_main(int argc, char **argv) {
     // TODO: expose sample list and regions to CLI.
-
-    if (argc != 2) {
+    string samplemissing;
+    int c;
+    while ((c = getopt(argc, argv, "s:h")) > 0) {
+        switch (c) {
+            case 's':
+                samplemissing = optarg;
+                if (samplemissing == "-") {
+                    samplemissing = "/dev/fd/1";
+                }
+                break;
+            case '?':
+            case 'h':
+                usage();
+                return(EXIT_FAILURE);
+        }
+    }
+    if (argc - optind != 1) {
         usage();
         return EXIT_FAILURE;
     }
-    string filename = argv[1];
-
-    if (filename == "--help" || filename == "-h") {
-        usage();
-        return EXIT_FAILURE;
-    }
+    string filename = argv[optind];
 
     string region;
-    vector<string> samples;
     string samplecsv;
     bool success = true;
     map<int, uint64_t> af_hist;
     map<int, uint64_t> miss_hist;
     map<int, uint64_t> qual_hist;
     map<int, uint64_t> dp_hist;
+    map<int, uint64_t> sample_missingness;
 
     for (int i=0; i<=100; i++) {
         af_hist[i] = 0;
@@ -75,9 +90,19 @@ int bcfhist_main(int argc, char **argv) {
         success = false;
     }
     bcf_hdr_t *header = sr->readers[0].header;
+    if (header == NULL) success = false;
+
+    vector<string> samples;
+    if (success) {
+        int32_t nsamp = bcf_hdr_nsamples(header);
+        for (int32_t i = 0; i < nsamp; i++) {
+            samples.push_back(header->samples[i]);
+        }
+    }
 
     int32_t *buffer = NULL;
     int32_t buffersz = 0;
+    uint64_t nsnp = 0;
     while (success && bcf_sr_next_line(sr)) {
         bcf1_t *record = bcf_sr_get_line(sr,0);
         int32_t nentry = bcf_get_format_int32(header, record, "GT", &buffer, &buffersz);
@@ -85,11 +110,14 @@ int bcfhist_main(int argc, char **argv) {
         int nalt = 0;
         if (nentry < 0) {
             success = false;
+            continue;
         }
+        nsnp++;
         for (int32_t i=0; i<nentry; i += 2) {
+            int sample = i / 2;
             int32_t a=bcf_gt_allele(buffer[i]), b=bcf_gt_allele(buffer[i+1]);
-            if (a < 0) nmiss++; else nalt += a;
-            if (b < 0) nmiss++; else nalt += b;
+            if (a < 0) {nmiss++; sample_missingness[sample]++;} else nalt += a;
+            if (b < 0) {nmiss++; sample_missingness[sample]++;} else nalt += b;
         }
         float miss = (float)nmiss / nentry;
         float af = (float)nalt / (nentry - nmiss);
@@ -110,11 +138,6 @@ int bcfhist_main(int argc, char **argv) {
             }
         }
 
-
-        //cerr << bcf_hdr_id2name(header, record->rid) << "\t" << record->pos << "\t" << nmiss << "\t" << nalt << "\t" << nentry - nmiss << "\t" << af << "\t" << miss << endl;
-
-        //af_hist(af);
-        //mis_hist(miss);
         af_hist[percent(af)]++;
         miss_hist[percent(miss)]++;
         qual_hist[(int)round(record->qual)]++;
@@ -122,6 +145,7 @@ int bcfhist_main(int argc, char **argv) {
     }
     if (sr->errnum) {
         cerr << "Error: " << bcf_sr_strerror(sr->errnum) << endl;
+        success = false;
     }
 
     if (success) {
@@ -137,6 +161,13 @@ int bcfhist_main(int argc, char **argv) {
         }
         for (const auto &[key, value]: dp_hist) {
             cout << "dp\t" << key << "\t" << value  << endl;
+        }
+        if (samplemissing.size()) {
+            ofstream out(samplemissing);
+            out << "sample\tmissing_prop\n";
+            for (const auto &[key, value]: sample_missingness) {
+                out << samples[key] << "\t" << ((float)value/2)/nsnp << endl;
+            }
         }
     }
 
